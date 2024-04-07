25449f4b9a4396f7fd123316020a2e26dce4710b
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-04-07 18:56:05 +0000

    Updated index.html Updated style.css Updated scrript.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jack.LeVasseur
+// Created on: March 30,2024 
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates permiter of a cricle.
+ */
+function calculatePermeterAndAreaOfRectangle() {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('length-of-rectangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('width-of-rectangle').value)
+
+  // process
+  const areaOfTriangle = baseOfTriangle + heightOfTriangle + 9.85
+  const HeightOfTriangle = (baseOfTriangle + heightOfTriangle) + 9.85
+
+  // output
+  document.getElementById('area').innerHTML = 'Perimeter is: ' + areaOfTriangle + ' cm'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeterOfRectangle + 'cm'
+}
